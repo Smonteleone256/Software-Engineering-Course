0a920f5e121e5bdebea989ce0ae8688b715345e5
Smonteleone256 <smonte.lion@gmail.com>
2025-07-23 17:10:37 -0400

    Project Finished API 7/23/25

--- giphy-party-starter/index.js
+++ giphy-party-starter/index.js
@@ -1,10 +1,15 @@
-// look back at the <readme.md> file for some hints //
-// working API key //
 const giphyApiKey = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
 let searchButton = document.getElementById("search");
 let removeButton = document.getElementById("remove");
 let searchBar = document.getElementById("type");
 let searchTerms = "";
+let gifRepo = document.getElementById("GIFList");
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 document.addEventListener("DOMContentLoaded", function () {
   searchBar.addEventListener("submit", function (event) {
@@ -12,16 +17,20 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   searchButton.addEventListener("click", async function (event) {
     event.preventDefault();
-    console.log("clicked");
     console.log(searchBar.value);
     searchTerms = searchBar.value;
     const response = await fetch(
-      `https://api.giphy.com/v1/gifs/search?q=${searchTerms}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`
+      `https://api.giphy.com/v1/gifs/search?q=${searchTerms}&api_key=${giphyApiKey}`
     );
     const data = await response.json();
     console.log(data);
+    const newGif = new Image(151, 151);
+    newGif.src = data.data[getRandomInt(0, 50)].url;
+    gifRepo.appendChild(newGif);
   });
   removeButton.addEventListener("click", function () {
     location.reload();
   });
 });
+
+//data[getRandomInt(0, 50)].url
